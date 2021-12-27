7241efbcdd31d947a0b4166eececf9a9d49abcf2
Norlan Tibanear <ntibanear@gmail.com>
2021-12-27 12:40:38 -0500

    Initial Commit

---
+++ Tasktracker/TasktrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasktrackerApp.swift
+//  Tasktracker
+//
+//  Created by Norlan Tibanear on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct TasktrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
