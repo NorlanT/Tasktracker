eaa727abbcce9018435ef2f678a397886f4f5781
Norlan Tibanear <ntibanear@gmail.com>
2021-12-27 18:43:56 -0500

    Basic coreData setup

--- Tasktracker/ContentView.swift
+++ Tasktracker/ContentView.swift
@@ -9,14 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .foregroundColor(.red)
-    }
+       TabView {
+          HomeView()
+             .tabItem {
+                Image(systemName: "house")
+                Text("Home")
+             }
+          
+          ProjectView(showClosedProjects: false)
+             .tabItem {
+                Image(systemName: "list.bullet")
+                Text("Open")
+             }
+          
+          ProjectView(showClosedProjects: true)
+             .tabItem {
+                Image(systemName: "checkmark")
+                Text("Closed")
+             }
+          
+       }
+    }//body
 }
 
 struct ContentView_Previews: PreviewProvider {
+   static var dataController = DataController.preview
+   
     static var previews: some View {
-        ContentView()
+       ContentView()
+          .environment(\.managedObjectContext, dataController.container.viewContext)
+          .environmentObject(dataController)
     }
 }
---
+++ Tasktracker/HomeView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeView.swift
+//  Tasktracker
+//
+//  Created by Norlan Tibanear on 12/27/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+   
+   @EnvironmentObject var dataController: DataController
+   
+    var body: some View {
+       NavigationView {
+          VStack {
+             Button("Add Data") {
+                dataController.deleteAll()
+                try? dataController.createSampleData()
+             }
+          }
+          .navigationTitle("Home")
+          
+       }//Nav
+    }//body
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
